aae0a255672c47f34c1aa1bbcc8658eb52906657
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-03-03 01:47:32 +0200

    Д.з. 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const body = document.querySelector('body');
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
 
+let isActive = false;
+stop.setAttribute('disabled', true);
+
+start.addEventListener('click', bodyStyle);
+stop.addEventListener('click', btnStop);
+
+function bodyStyle() { 
+    if (isActive) {
+        return;
+    }
+    start.setAttribute('disabled', true);
+    stop.removeAttribute('disabled');
+    isActive = true;
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+};
+
+function btnStop() {
+    clearInterval(timerId);
+    isActive = false;
+    stop.setAttribute('disabled', true);
+    start.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
